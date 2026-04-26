527db604532eb7b08341e3b714fc20eedaf3b23f
gthomakos14 <gthomakos14@gmail.com>
2026-04-26 12:37:42 -0400

    made game_tracker object oriented

--- game_tracker.py
+++ game_tracker.py
@@ -5,18 +5,34 @@ BASE_URL = "https://statsapi.mlb.com/api/v1"
 # TODO: Future functionality can have the team ID in the config if other teams become important
 YANKEES_ID = 147
 
-def get_game_pk(today=''):
-    # Conditional allows other dates to be passed
-    if len(today) == 0:
-        today = date.today().strftime("%Y-%m-%d")
-    url = f"{BASE_URL}/schedule?sportId=1&teamId={YANKEES_ID}&date={today}"
-    result = requests.get(url)
-    assert result.status_code == 200, f'Status code on game Pk fetch was {result.status_code}'
+class Tracker:
+    def __init__(self, game_pk=None):
+        if game_pk is not None:
+            self.game_pk = game_pk
+        else:
+            today = date.today().strftime("%Y-%m-%d")
+            url = f"{BASE_URL}/schedule?sportId=1&teamId={YANKEES_ID}&date={today}"
+            result = requests.get(url)
+            assert result.status_code == 200, f'Status code on game Pk fetch was {result.status_code}'
 
-    body = result.json()
-    try:
-        # TODO: This does not support double headers
-        game_pk = body['dates'][0]['games'][0]['gamePk']
-        return game_pk
-    except (KeyError, IndexError):
-        return None
+            body = result.json()
+            try:
+                # TODO: This does not support double headers
+                game_pk = body['dates'][0]['games'][0]['gamePk']
+                return game_pk
+            except (KeyError, IndexError):
+                return None
+        self.game_status = 'INACTIVE'
+        self.scoring_plays = []
+
+
+    def get_game_pk(self):
+        return self.game_pk
+    
+
+    def get_scoring_plays(self):
+        return self.scoring_plays
+    
+
+    def set_scoring_plays(self, new_scoring_plays):
+        self.scoring_plays = new_scoring_plays
