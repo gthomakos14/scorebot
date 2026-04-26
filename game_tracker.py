aeed5cf46b5a610bcc222bcbda65d6d2c5bed28c
Glenn <Glenn Thomakos>
2026-04-26 19:20:40 -0400

    switched to mets for testing, made the team ID variable name team agnostic, and accounted for absent keys in scoring play fetch

--- game_tracker.py
+++ game_tracker.py
@@ -4,7 +4,38 @@ from notifier import send_score_update
 
 BASE_URL = "https://statsapi.mlb.com/api/v1"
 # TODO: Future functionality can have the team ID in the config if other teams become important
-YANKEES_ID = 147
+TEAM_ID = 121
+
+# {'Athletics': 133,
+#  'Pittsburgh Pirates': 134,
+#  'San Diego Padres': 135,
+#  'Seattle Mariners': 136,
+#  'San Francisco Giants': 137,
+#  'St. Louis Cardinals': 138,
+#  'Tampa Bay Rays': 139,
+#  'Texas Rangers': 140,
+#  'Toronto Blue Jays': 141,
+#  'Minnesota Twins': 142,
+#  'Philadelphia Phillies': 143,
+#  'Atlanta Braves': 144,
+#  'Chicago White Sox': 145,
+#  'Miami Marlins': 146,
+#  'New York Yankees': 147,
+#  'Milwaukee Brewers': 158,
+#  'Los Angeles Angels': 108,
+#  'Arizona Diamondbacks': 109,
+#  'Baltimore Orioles': 110,
+#  'Boston Red Sox': 111,
+#  'Chicago Cubs': 112,
+#  'Cincinnati Reds': 113,
+#  'Cleveland Guardians': 114,
+#  'Colorado Rockies': 115,
+#  'Detroit Tigers': 116,
+#  'Houston Astros': 117,
+#  'Kansas City Royals': 118,
+#  'Los Angeles Dodgers': 119,
+#  'Washington Nationals': 120,
+#  'New York Mets': 121}
 
 class Tracker:
     def __init__(self, game_pk=None):
@@ -12,7 +43,7 @@ class Tracker:
             self.game_pk = game_pk
         else:
             today = date.today().strftime("%Y-%m-%d")
-            url = f"{BASE_URL}/schedule?sportId=1&teamId={YANKEES_ID}&date={today}"
+            url = f"{BASE_URL}/schedule?sportId=1&teamId={TEAM_ID}&date={today}"
             result = requests.get(url)
             assert result.status_code == 200, f'Status code on game Pk fetch was {result.status_code}'
 
@@ -64,7 +95,7 @@ class Tracker:
                 "play_index": play["about"]["atBatIndex"],
             }
             for play in all_plays
-            if play["about"]["isScoringPlay"]
+            if play["about"].get('isScoringPlay', False)
         ]
     
 
